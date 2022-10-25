a3819be49446a60ad5c7199f91c77cb880fe1564
Sobia Arshad <sobiaarshad02@gmail.com>
2022-10-26 01:58:29 +0400

    Create Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,10 @@
+Alien_color= 'Green'
+if Alien_color == 'Green':
+  print ("You just earned 5 points for shooting the alien")
+else:
+    print("You just earned 10 points")
+Alien_color= 'Yellow'
+if Alien_color == 'Green':
+ print ("You just earned 5 points")
+else: 
+  print("You just earned 10 points")

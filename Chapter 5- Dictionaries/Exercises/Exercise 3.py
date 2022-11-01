9402072a272162f8cb17fc0fed92bfd8b308ca53
Sobia Arshad <sobiaarshad02@gmail.com>
2022-11-01 23:11:49 +0400

    Create Exercise 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -0,0 +1,7 @@
+Glossary= {'List': 'List is an ordered and changeable collection of data objects.','Tuples': 'An immutable sequence.',
+'String': 'Sequence of characters that is used as data.','Comment': 'Notes of explanation within the program.',
+'Function': 'Piece of prewritten code that performs an operation.','Boolean': 'Represents one of two values:True or False.',
+'Argument': 'Data given to function.','Variable':'Containers which store values.',
+'Float':'Returns a floating point number from a number or a string.','Input':'Input funtion allows user input.'}
+for word, definition in Glossary.items():
+ print("\n" + word + ": " + definition)

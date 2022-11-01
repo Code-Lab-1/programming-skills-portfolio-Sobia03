5cc17dbef668ea3d0a381c942343c4fb84bafa29
Sobia Arshad <sobiaarshad02@gmail.com>
2022-11-02 00:08:27 +0400

    Create Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,10 @@
+guests=['Marilyn', 'Kate', 'Diana'] 
+print ((guests[0]) + " kindly join me for dinner.")
+print ((guests[1]) + " kindly join me for dinner.")
+print ((guests[2]) + " kindly join me for dinner.")
+print ((guests[0]) + " can't make it to dinner.")
+del(guests[1])
+guests.insert(0, 'Elizabeth')
+print ((guests[0]) + " kindly join me for dinner.")
+print ((guests[1]) + " kindly join me for dinner.")
+print ((guests[2]) + " kindly join me for dinner.")

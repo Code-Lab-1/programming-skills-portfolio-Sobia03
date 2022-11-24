d4e8370084e8f8634b898660bb306f1a97aebc3b
Sobia Arshad <sobiaarshad02@gmail.com>
2022-11-25 03:53:21 +0400

    Create Exercise 10.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 10.py
@@ -0,0 +1,7 @@
+Bucket_list=["Travel the world","Go bunjee jumping","Go to a concert","Ride a rollercoster"]
+print(Bucket_list)
+print("\nAlphabetical:")
+print(sorted(Bucket_list))
+print (Bucket_list)
+del(Bucket_list[1])
+print(Bucket_list)
